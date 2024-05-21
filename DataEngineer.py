b44b0a90d6605105262c08d3560c546a5d364851
moises marquina <moisesmarquinaj@gmail.com>
2024-05-21 13:51:52 -0400

    ETL_1

---
+++ DataEngineer.py
@@ -0,0 +1,14 @@
+i = int(input())
+# print(f'ingresa un número par: {i}')
+
+def Num_par():
+    while (i % 2) != 0:
+        print('Error')
+        print(f'Ingresa un número par:')
+        i = int(input())
+
+    else:
+        print('Perfecto, num par')
+
+def otro():
+    pass
